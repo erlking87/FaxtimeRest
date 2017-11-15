132b5fdebfa75bf2809b3794dcee0801b16b1c5d
prochods <prochods@gmail.com>
2017-11-15 14:15:18 +0900

    ALIMTALK 적용

--- FaxtimeRest/handlers/v1/alimtalk/send.js
+++ FaxtimeRest/handlers/v1/alimtalk/send.js
@@ -1,18 +1,19 @@
 'use strict';
 
- var repository = require('../../../lib/alimtalkRepository');
+var repository = require('../../../lib/alimtalkRepository');
 
- module.exports = {
-     get: function ALIMTALK_TBL_REQUEST_GET(req, res) {
-         repository.selectSendMsg(req, res);
-     },
-     post: function ALIMTALK_TBL_REQUEST_POST(req, res) {
-         repository.createSendMsg(req, res);
-     },
-     put: function ALIMTALK_TBL_REQUEST_PUT(req, res) {
-         repository.updateSendMsg(req, res);
-     },
-     delete: function ALIMTALK_TBL_REQUEST_DELETE(req, res) {
-         repository.deleteSendMsg(req, res);
-     }
- };
+module.exports = {
+    get: function ALIMTALK_TBL_REQUEST_GET(req, res) {
+        repository.selectSendMsg(req, res);
+    },
+    post: function ALIMTALK_TBL_REQUEST_POST(req, res) {
+        //console.log(">>>>>>>[send.js] " + JSON.stringify(req.body));
+        repository.createSendMsg(req, res);
+    },
+    put: function ALIMTALK_TBL_REQUEST_PUT(req, res) {
+        repository.updateSendMsg(req, res);
+    },
+    delete: function ALIMTALK_TBL_REQUEST_DELETE(req, res) {
+        repository.deleteSendMsg(req, res);
+    }
+};
