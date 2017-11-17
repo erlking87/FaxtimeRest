637e22469e007304b3c90e804a3db8e495bb9c37
erlking87 <songhj@SONGHJ-PC>
2017-11-17 14:32:15 +0900

    원본 commit

--- FaxtimeRest/handlers/v1/faxtime/mms/send.js
+++ FaxtimeRest/handlers/v1/faxtime/mms/send.js
@@ -1,20 +1,18 @@
 'use strict';
 
- var repository = require('../../../../lib/faxtimeMmsRepository');
+var repository = require('../../../../lib/faxtimeMmsRepository');
 
- module.exports = {
-     get: function FAXTIME_SDK_MMS_SEND_GET(req, res) {
-         repository.selectSendMsg(req, res);
-     },
-     post: function FAXTIME_SDK_MMS_SEND_POST(req, res) {
-		 req.connection.setTimeout(600000000000000000000000000000000);
-		 res.connection.setTimeout(600000000000000000000000000000000);
-         repository.createSendMsg(req, res);
-     },
-     put: function FAXTIME_SDK_MMS_SEND_PUT(req, res) {
-         repository.updateSendMsg(req, res);
-     },
-     delete: function FAXTIME_SDK_MMS_SEND_DELETE(req, res) {
-         repository.deleteSendMsg(req, res);
-     }
- };
+module.exports = {
+    get: function FAXTIME_SDK_MMS_SEND_GET(req, res) {
+        repository.selectSendMsg(req, res);
+    },
+    post: function FAXTIME_SDK_MMS_SEND_POST(req, res) {
+        repository.createSendMsg(req, res);
+    },
+    put: function FAXTIME_SDK_MMS_SEND_PUT(req, res) {
+        repository.updateSendMsg(req, res);
+    },
+    delete: function FAXTIME_SDK_MMS_SEND_DELETE(req, res) {
+        repository.deleteSendMsg(req, res);
+    }
+};
