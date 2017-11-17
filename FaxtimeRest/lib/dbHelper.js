f7691df0acf77948fe7c7cf2c654cabe29d42982
songhj <songhj@SONGHJ-PC>
2017-11-17 09:33:19 +0900

    원본 commit

--- FaxtimeRest/lib/dbHelper.js
+++ FaxtimeRest/lib/dbHelper.js
@@ -4,10 +4,9 @@ var dbConfig = require('./dbConfig');
 var sql = require('mssql');
 var fs = require('fs');
 var dateFormat = require('dateformat');
-var objAsync = require('async');
 
-var LogWriter = function(sqlText) {
-    if (!fs.existsSync("./log")){
+var LogWriter = function (sqlText) {
+    if (!fs.existsSync("./log")) {
         fs.mkdirSync("./log");
     } else {
         var stats = fs.lstatSync("./log");
@@ -34,102 +33,26 @@ exports.sqlDecode = function (id, object, defaultValue) {
     return object[id];
 }*/
 
-exports.sqlSelectParameters = function(req) {
-    var 
-    result = {}
-    , findObject = function (id, object, defaultValue) {
-        if(typeof id === "undefined" || null == id
-            || typeof object === "undefined" || null == object
-        ) return (typeof defaultValue === "undefined") ? "" : defaultValue;
-        if(typeof object[id] === "undefined" || null == object[id]
-        ) return (typeof defaultValue === "undefined") ? "" : defaultValue;
-        return object[id];
-    };
-    
+exports.sqlSelectParameters = function (req) {
+    var
+        result = {}
+        , findObject = function (id, object, defaultValue) {
+            if (typeof id === "undefined" || null == id
+                || typeof object === "undefined" || null == object
+            ) return (typeof defaultValue === "undefined") ? "" : defaultValue;
+            if (typeof object[id] === "undefined" || null == object[id]
+            ) return (typeof defaultValue === "undefined") ? "" : defaultValue;
+            return object[id];
+        };
+
     result["agentKey"] = req.get("Agent-Key") || "";
     result["user"] = findObject("user", req.query, null);
     result["pageSize"] = 10;
     result["currentPage"] = findObject("page", req.query, 1);
-    //////////////////////////////////////////////////////////
-    result["fdate"] = findObject("fdate", req.query, null);
-    result["tdate"] = findObject("tdate", req.query, null);
-    result["ntblusersid"] = findObject("ntblusersid", req.query, null);
-    result["message_type"] = findObject("message_type", req.query, null);
-    result["msgid"] = findObject("msgid", req.query, null);
-    //////////////////////////////////////////////////////////
     result["startIndex"] = (result["currentPage"] - 1) * result["pageSize"] + 1;
     result["endIndex"] = result["currentPage"] * result["pageSize"];
     return result;
 }
-
-exports.sqlNewSelect = function (sqlText, res, id, callback) {
-
-    LogWriter(sqlText);
-    console.log("QUERY -> " + sqlText);
-
-	objAsync.waterfall([
-		function (callback) {
-		    console.log("DB Connection Start ....");
-	    	//var gggg = dbHelper.sqlNewSelect(sqlSelText, res);
-	    	//TODO 여기에서 시퀀스만 받음 되는데...
-		    const pool = new sql.ConnectionPool(dbConfig, err => {
-	        	pool.request().query(sqlText, (err, result) => {
-	        	    // 에러인 경우 빈객체로 리턴한다.
-	        	    if (typeof err !== 'undefined' && null != err) {
-					    console.log("ERROR@@@ -> " + err);
-	        	        return;
-	        	    }
-	        	    var val = {};
-	        	    val["status"] = (0 == result.rowsAffected[0]) ? "204" : "200";
-	        	    val["description"] = (0 == result.rowsAffected[0]) ? "콘텐츠 없음" : "성공";
-	        	    val["affected"] = result.rowsAffected[0];
-	        	    if (typeof id !== 'undefined' && null != id) {
-	        	        // 결과가 없을때 에러난다.
-	        	        val["datas"] = (0 < result.rowsAffected[0]) ? result.recordset[0] : null;
-	        	        
-	        	    } else {
-	        	        val["datas"] = (0 < result.rowsAffected[0]) ? result.recordsets[0] : [];
-	        	    }
-				    console.log("Sequence Cpmplete !!!!!" + JSON.stringify(result.recordset[0]));
-	        	    return 123456789;
-				});
-			    console.log("DB Connection OK !!!");
-		    });
-		    pool.on('error', err => {
-			    console.log("DB Connect ERROR");
-		        return;
-		    });
-	        //pool.request().query(sqlText, (err, result) => {
-	        //    // 에러인 경우 빈객체로 리턴한다.
-	        //    if(typeof err !== 'undefined' && null != err) {
-	        //        return res.send({
-	        //                "status" : "500",
-	        //                "description" : "내부 서버 오류"
-	        //        });
-	        //    }
-	        //    var val = {};
-	        //    val["status"] = (0 == result.rowsAffected[0]) ? "204" : "200";
-	        //    val["description"] = (0 == result.rowsAffected[0]) ? "콘텐츠 없음" : "성공";
-	        //    val["affected"] = result.rowsAffected[0];
-	        //    if(typeof id !== 'undefined' && null != id) {
-	        //        // 결과가 없을때 에러난다.
-	        //        val["datas"] = (0 < result.rowsAffected[0]) ? result.recordset[0] : null;
-	        //        
-	        //    } else {
-	        //        val["datas"] = (0 < result.rowsAffected[0]) ? result.recordsets[0] : [];
-	        //    }
-			//    console.log("Sequence Cpmplete !!!!!");
-	        //    return 123456789;
-	        //    //callback(123456789);
-	        //});
-		    console.log("DB Connect Complete");
-
-
-	    	callback(null, callback);
-		}
-	], function (err, result) { console.log("seq end ===> " + err); }
-	);
-};
 
 exports.sqlSelect = function (sqlText, res, id) {
 
@@ -137,31 +60,29 @@ exports.sqlSelect = function (sqlText, res, id) {
 
     const pool = new sql.ConnectionPool(dbConfig, err => {
         pool.request()
-        .query(sqlText, (err, result) => {
-            // 에러인 경우 빈객체로 리턴한다.
-            if(typeof err !== 'undefined' && null != err) {
-		    	console.log(err);
-                return res.send({
-                        "status" : "500",
-                        "description" : "내부 서버 오류"
-                });
-            }
-            var val = {};
-            val["status"] = (0 == result.rowsAffected[0]) ? "204" : "200";
-            val["description"] = (0 == result.rowsAffected[0]) ? "콘텐츠 없음" : "성공";
-            val["affected"] = result.rowsAffected[0];
-            if(typeof id !== 'undefined' && null != id) {
-                // 결과가 없을때 에러난다.
-                val["datas"] = (0 < result.rowsAffected[0]) ? result.recordset[0] : null;
-                
-            } else {
-                val["datas"] = (0 < result.rowsAffected[0]) ? result.recordsets[0] : [];
-            }
-            return res.send(val);
-        });
+            .query(sqlText, (err, result) => {
+                // 에러인 경우 빈객체로 리턴한다.
+                if (typeof err !== 'undefined' && null != err) {
+                    return res.send({
+                        "status": "500",
+                        "description": "내부 서버 오류"
+                    });
+                }
+                var val = {};
+                val["status"] = (0 == result.rowsAffected[0]) ? "204" : "200";
+                val["description"] = (0 == result.rowsAffected[0]) ? "콘텐츠 없음" : "성공";
+                val["affected"] = result.rowsAffected[0];
+                if (typeof id !== 'undefined' && null != id) {
+                    // 결과가 없을때 에러난다.
+                    val["datas"] = (0 < result.rowsAffected[0]) ? result.recordset[0] : null;
+
+                } else {
+                    val["datas"] = (0 < result.rowsAffected[0]) ? result.recordsets[0] : [];
+                }
+                return res.send(val);
+            });
     });
     pool.on('error', err => {
-    	console.log(err);
         return res.send({
             "status": "500",
             "description": "내부 서버 오류"
@@ -169,7 +90,7 @@ exports.sqlSelect = function (sqlText, res, id) {
     });
 };
 
-exports.sqlExecute = function (sqlText, seqNo, res) {
+exports.sqlExecute = function (sqlText, res) {
     var affected = 0;
     const pool = new sql.ConnectionPool(dbConfig, err => {
         const transaction = new sql.Transaction(pool);
@@ -177,7 +98,7 @@ exports.sqlExecute = function (sqlText, seqNo, res) {
             if (typeof err !== 'undefined' && null != err) {
                 return res.send({
                     "status": "500",
-                    "description": "내부 서버 오류0000"
+                    "description": "내부 서버 오류"
                 });
             }
             const request = new sql.Request(transaction);
@@ -186,11 +107,10 @@ exports.sqlExecute = function (sqlText, seqNo, res) {
                     if (typeof err !== 'undefined' && null != err) {
                         return res.send({
                             "status": "500",
-                            "description": "내부 서버 오류1111"
+                            "description": "내부 서버 오류"
                         });
                     }
                     return res.send({
-                        "msgIds": ((0 == affected) ? "0" : seqNo),
                         "status": ((0 == affected) ? "204" : "200"),
                         "description": ((0 == affected) ? "콘텐츠 없음" : "성공"),
                         "affected": affected
@@ -203,15 +123,13 @@ exports.sqlExecute = function (sqlText, seqNo, res) {
                     done();
                     return;
                 } else {
-                    //console.log("Insert Query -> " + sqlText[i]);
                     LogWriter(sqlText[i]);
                     request.query(sqlText[i], (err, result) => {
                         if (typeof err !== 'undefined' && null != err) {
-                    		//console.log("Insert Query err -> " + err);
                             transaction.rollback(err => {
                                 return res.send({
                                     "status": "500",
-                                    "description": "내부 서버 오류2222"
+                                    "description": "내부 서버 오류"
                                 });
                             });
                             return;
@@ -224,124 +142,11 @@ exports.sqlExecute = function (sqlText, seqNo, res) {
             nextItem(0);
         });
     });
-    
+
     pool.on('error', err => {
         return res.send({
-            "status" : "500",
-            "description": "내부 서버 오류3333"
+            "status": "500",
+            "description": "내부 서버 오류"
         });
     });
 };
-
-
-exports.sqlUpdate = function (sqlText, res) {
-    var affected = 0;
-    const pool = new sql.ConnectionPool(dbConfig, err => {
-        const transaction = new sql.Transaction(pool);
-        transaction.begin(err => {
-            if(typeof err !== 'undefined' && null != err) {
-                return res.send({
-                    "status" : "500",
-                    "description": "내부 서버 오류0000"
-                });
-            }
-            const request = new sql.Request(transaction);
-            function done() {
-                transaction.commit(err => {
-                    if(typeof err !== 'undefined' && null != err) {
-                        return res.send({
-                            "status" : "500",
-                            "description" : "내부 서버 오류1111"
-                        });
-                    }
-                    return res.send({
-                        "status" : ((0 == affected) ? "204" : "200"),
-                        "description" : ((0 == affected) ? "콘텐츠 없음" : "성공"),
-                        "affected" : affected
-                    });
-                });
-            }
-            var nextItem = function(i) {
-                if (i >= sqlText.length) {
-                    // All done
-                    done();
-                    return;
-                } else {
-                    //console.log("Insert Query -> " + sqlText[i]);
-                    LogWriter(sqlText[i]);
-                    request.query(sqlText[i], (err, result) => {
-                        if(typeof err !== 'undefined' && null != err) {
-                    		//console.log("Insert Query err -> " + err);
-                            transaction.rollback(err => {
-                                return res.send({
-                                    "status" : "500",
-                                    "description" : "내부 서버 오류2222"
-                                });
-                            });
-                            return;
-                        }
-                        affected = affected + result.rowsAffected[0];
-                        nextItem(i + 1);
-                    });
-                }
-            }
-            nextItem(0);
-        });
-    });
-    
-    pool.on('error', err => {
-        return res.send({
-            "status" : "500",
-            "description" : "내부 서버 오류3333"
-        });
-    });
-};
-
-exports.sqlNewSelect1 = function (sqlText) {
-
-    LogWriter(sqlText);
-    console.log("QUERY -> " + sqlText);
-    var val = {};
-    val["status111"] = "200";
-
-    const pool = new sql.ConnectionPool(dbConfig, err => {
-        pool.request().query(sqlText, (err, result) => {
-            // 에러인 경우 빈객체로 리턴한다.
-            if (typeof err !== 'undefined' && null != err) {
-                return "188 ERROR";
-            }
-            //var val = {};
-            val["status"] = (0 == result.rowsAffected[0]) ? "204" : "200";
-            val["description"] = (0 == result.rowsAffected[0]) ? "콘텐츠 없음" : "성공";
-            val["affected"] = result.rowsAffected[0];
-            if (typeof id !== 'undefined' && null != id) {
-                // 결과가 없을때 에러난다.
-                val["datas"] = (0 < result.rowsAffected[0]) ? result.recordset[0] : null;
-                
-            } else {
-                val["datas"] = (0 < result.rowsAffected[0]) ? result.recordsets[0] : [];
-            }
-            //return val;
-            if (err) {
-		    	val["status111"] = "ERROR";
-        		//return "ERROR";
-	    		return JSON.stringify(val);
-                // 결과가 없을때 에러난다.
-                
-            } else {
-		    	val["status111"] = "OK";
-        		//return "ERROR";
-	    		return JSON.stringify(val);
-            }
-		    return JSON.stringify(val);
-        });
-    });
-    pool.on('err', err => {
-    	val["status111"] = "ERROR####";
-        //return "ERROR";
-	    return JSON.stringify(val);
-    });
-
-    return JSON.stringify(val);
-};
-
--- FaxtimeRest/server.js
+++ FaxtimeRest/server.js
@@ -1,123 +1,31 @@
-﻿'use strict';
-
-var port = process.env.PORT || 8000; // first change
-//var port = 8000;
-var http = require('http');
-var express = require('express');
-var bodyParser = require('body-parser');
-var swaggerize = require('swaggerize-express');
-var swaggerUi = require('swaggerize-ui'); // second change
-var path = require('path');
-
-var multer = require('multer');
-//var storage = multer.memoryStorage()
-//var upload = multer({ storage: storage })
-var upload = multer({ dest: 'uploads/' });
-var repository = require('./lib/fileRepository');
-
-var app = express();
-
-var server = http.createServer(app);
-
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/v1/others/file/upload', upload.single('file'), (req, res, next) => {
-    repository.uploadFile(req, res, next);
-});
-
-app.use(swaggerize({
-    api: path.resolve('./config/swagger.json'), // third change
-    handlers: path.resolve('./handlers'),
-    docspath: '/swagger' // fourth change
-}));
-
-// change four
-app.use('/docs', swaggerUi({
-    docs: '/swagger'
-}));
-
-server.listen(port, function () { // fifth and final change
-    console.log("start server port : " + port);
-	server.timeout = 6000000000;
-});
-
-app.get('/', (req, res) => {
-    console.log("/start");
-    res.send('api server\n');
-});
-
-app.post('/v1/sender/sms', (req, res) => {
-    var userIP = getUserIP(req);
-
-    var id = req.body.id;
-    var pw = req.body.pw;
-    var token = req.body.token;
-
-    console.log("id : " + req.body.id);
-    console.log("pw : " + req.body.pw);
-    console.log("token : " + req.body.token);
-    console.log("userIp : " + userIP);
-
-    if (id != null && pw != null && token != null) {
-
-        let result = {
-            "code": "success",
-            "data": null,
-            "message": null
-        }
-
-        res.json(result);
-    } else {
-
-        let result = {
-            "code": "fail",
-            "data": null,
-            "message": "invalid message"
-        }
-
-        res.json(result);
+﻿/*
+    실서버
+*/
+var dbConfig = {
+    user: 'faxtime',
+    password: 'test2016!',
+    server: 'faxtimedb.database.windows.net',
+    database: 'faxtime01',
+    connectionTimeout: 300000,
+    requestTimeout: 300000,
+    options: {
+        encrypt: true
     }
-
-});
-
-app.get('/v1', function (req, res) {
-    console.log(req.params.id);
-    return res.send('test');
-});
-
+};
 
 /*
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+    테스트서버
+*/
+/*
+var dbConfig = {
+    user: 'taiho',
+    password: 'xkdlgh123!',
+    server: '150.1.4.61',
+    database: 'ufws01',
+    options: {
+        encrypt: true
+    }
+};
 */
 
-
-
-function getUserIP(req) {
-    var ipAddress;
-
-    if (!!req.hasOwnProperty('sessionID')) {
-        ipAddress = req.headers['x-forwarded-for'];
-    } else {
-        if (!ipAddress) {
-            var forwardedIpsStr = req.header('x-forwarded-for');
-
-            if (forwardedIpsStr) {
-                var forwardedIps = forwardedIpsStr.split(',');
-                ipAddress = forwardedIps[0];
-            }
-            if (!ipAddress) {
-                ipAddress = req.connection.remoteAddress;
-            }
-        }
-    }
-    return ipAddress;
-}
+module.exports = dbConfig;
